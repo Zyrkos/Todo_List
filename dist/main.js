0f5f84400453fd67c218d1d2ca2d90f388aa1743
Zyrkos <pedrocastro1995@hotmail.com>
2023-04-11 23:32:01 +0100

    initial commit

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+consloe.log("hello world")
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLDBCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kb19saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNsb2UubG9nKFwiaGVsbG8gd29ybGRcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
