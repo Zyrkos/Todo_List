85b3a3dea605dcd79adf77951cab7fba21ae68fb
Zyrkos <pedrocastro1995@hotmail.com>
2023-04-13 23:32:15 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import { compareAsc, format } from "date-fns";
 
 const domHandler = (() => {
-  const createForm = () => {
-    document.getElementById("new-btn");
-    
-  };
-
   const openTheForm = () => {
+    document.getElementById("new-btn");
     document.getElementById("popup-form").style.display = "block";
   };
 
@@ -14,16 +10,3 @@ const domHandler = (() => {
     document.getElementById("popup-form").style.display = "none";
   };
 })();
-
-function openForm() {
-  document.getElementById("loginPopup").style.display = "block";
-}
-function closeForm() {
-  document.getElementById("loginPopup").style.display = "none";
-  window.onclick = function (event) {
-    let modal = document.getElementById("popup-form");
-    if (event.target == modal) {
-      closeForm();
-    }
-  };
-}
